1367a3c69bb01871e2e09f78e44ac65768fa6f9c
mithon42 <mithonphilip@gmail.com>
2022-06-30 19:10:00 +0545

    Added New Message Hello.java file

--- Hello.java
+++ Hello.java
@@ -3,9 +3,15 @@ public class Hello {
     System.out.println("Hello World");
 
     greeting("Mithon Islam");
+    texting("Texting Message");
   }
 
   public static void greeting(String name) {
     System.out.println("Welcome " + name);
   }
+  
+  public static void texting(String message) {
+    System.out.println("Welcome " + message);
+  }
+
 }
