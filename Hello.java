f304eba6e36581276990b5bc3e88f386a0a25abf
mithon42 <mithonphilip@gmail.com>
2022-06-30 18:59:09 +0545

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello World");
+
+    greeting("Mithon Islam");
+  }
+
+  public static void greeting(String name) {
+    System.out.println("Welcome " + name);
   }
 }
